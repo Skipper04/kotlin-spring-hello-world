6e721e7393aca9c5842c1b16f2586368bbe16286
Andrei Kalikin <Skipper04@users.noreply.github.com>
2026-07-11 00:47:12 +0200

    Update HelloController to return HTML page

--- src/main/java/com/example/controller/HelloController.java
+++ src/main/java/com/example/controller/HelloController.java
@@ -1,17 +1,13 @@
 package com.example.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-import java.util.HashMap;
-import java.util.Map;
 
-@RestController
+@Controller
 public class HelloController {
 
     @GetMapping("/")
-    public Map<String, String> hello() {
-        Map<String, String> response = new HashMap<>();
-        response.put("message", "Hello World!");
-        return response;
+    public String hello() {
+        return "index";
     }
 }
